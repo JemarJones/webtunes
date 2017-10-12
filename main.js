fc2e163ff20de6d64a5aca10c757f584bee2566b
Jemar Jones <jemarkjones@gmail.com>
2017-10-12 12:57:29 -0400

    Trying to follow migration instructions

--- main.js
+++ main.js
@@ -40,8 +40,8 @@ var colors = require('colors');
      */
      self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8090;
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
